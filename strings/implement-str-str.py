1fa709ec7552d3d16b42d678e8ab717ac16250ea
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-07-18 19:21:26 -0400

    Create implement-str-str.py

---
+++ strings/implement-str-str.py
@@ -0,0 +1,16 @@
+def str_str(source: str, target: str) -> int:
+        if not target:
+            return 0
+        if not source:
+            return -1
+
+        for i in range(len(source)):
+            if i + len(target) > len(source):
+                break
+
+            for j in range(len(target)):
+                if source[i + j] != target[j]:
+                    break
+                if j == len(target) - 1:
+                    return i
+        return -1
